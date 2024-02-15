b843934b070c193b79404424ef7796b9f70b471e
Hashinology <m.sharif.uk@outlook.com>
2024-02-15 08:57:44 +0000

    + @Embedded Example in another Data class to represent an object that i would like to decompose into its subfields

--- app/src/main/java/com/hashinology/todoapp/models/DataClassModelExample.kt
+++ app/src/main/java/com/hashinology/todoapp/models/DataClassModelExample.kt
@@ -3,11 +3,16 @@ package com.hashinology.todoapp.models
 import androidx.room.Embedded
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import androidx.room.Relation
 
 @Entity
 data class DataClassModelExample(
     @PrimaryKey(autoGenerate = true)
     val userID: Int? = null,
     @Embedded
-    val tasksModel: TasksModel
+    val tasksModel: TasksModel,
+    @Relation(
+        parentColumn = "id",
+        entityColumn = "userID",
+    )
 )
